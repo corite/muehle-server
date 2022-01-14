7c34bfb71c1e41eb0ea9dcc3ec127501061951d1
Christian Ortlepp <christian.ortlepp@tutanota.com>
2022-01-14 09:51:07 +0100

    refactored stuff

--- src/main/java/ClientHandler.java
+++ src/main/java/ClientHandler.java
@@ -14,7 +14,6 @@ import java.util.stream.Collectors;
 
 public class ClientHandler implements Runnable{
     private final Socket clientSocket;
-    private Game game;
     private Player player;
     private final Logger logger = LoggerFactory.getLogger(getClass());
     public ClientHandler(Socket clientSocket) {
@@ -27,29 +26,28 @@ public class ClientHandler implements Runnable{
     }
 
     public Game getGame() {
-        if (game != null) {
-            return game;
-        } else {
-            if (getPlayer() != null) {
-                synchronized (Main.class) {
-                    Player self = getPlayer();
-                    ArrayList<Game> games = Main.getGames().stream()
-                            .filter(g -> self.equals(g.getPlayer2()) || self.equals(g.getPlayer1()))
-                            .collect(Collectors.toCollection(ArrayList::new));
-                    if (games.size() == 1) {
-                        this.setGame(games.get(0));
-                        return games.get(0);
-                    } else {
-                        logger.error("player {} was present in {} games, expected 1", self.getPlayerId(), games.size());
-                        return null;
-                    }
+        if (getPlayer() != null) {
+            synchronized (Main.class) {
+                Player self = getPlayer();
+                ArrayList<Game> games = Main.getGames().stream()
+                        .filter(g -> {
+                            synchronized (g) {
+                                return self.equals(g.getPlayer2()) || self.equals(g.getPlayer1());
+                            }
+                        })
+                        .collect(Collectors.toCollection(ArrayList::new));
+                if (games.size() == 1) {
+                    return games.get(0);
+                } else if (games.size() == 0){
+                    logger.debug("requested player {} was present in 0 games", self.getPlayerId());
+                    return null;
+                } else {
+                    logger.warn("player {} was present in {} games, expected 1 or 0",self.getPlayerId(),games.size());
+                    return null;
                 }
-            } else return null;
-        }
-    }
+            }
+        } else return null;
 
-    public void setGame(Game game) {
-        this.game = game;
     }
 
     public Player getPlayer() {
@@ -99,82 +97,86 @@ public class ClientHandler implements Runnable{
 
     private void handleGameAction(GameAction gameAction) {
         logger.debug("handling game action");
-        Player self = getPlayerReference(gameAction.getPlayer());
+        Game game = getGame();
+        Player self = getPlayerReference(gameAction.getPlayer(),game);
 
-        if (getGame() !=null) {
-            synchronized (this.getGame()) {
+        if (game !=null) {
+            synchronized (game) {
                 String message = "";
                 switch (gameAction.getType()) {
                     case PLACE -> {
                         try {
-                            getGame().placeStone(self, gameAction.getPlaceOrTakeCoordinate());
+                            game.placeStone(self, gameAction.getPlaceOrTakeCoordinate());
                         } catch (GameException e) {
                             message = getGameExceptionMessage(e);
                         }
                     }
                     case MOVE -> {
                         try {
-                            getGame().moveStone(self, gameAction.getFrom(), gameAction.getTo());
+                            game.moveStone(self, gameAction.getFrom(), gameAction.getTo());
                         } catch (GameException e) {
                             message = getGameExceptionMessage(e);
                         }
                     }
                     case TAKE -> {
                         try {
-                            getGame().takeStone(self, gameAction.getPlaceOrTakeCoordinate());
+                            game.takeStone(self, gameAction.getPlaceOrTakeCoordinate());
                         } catch (GameException e) {
                             message = getGameExceptionMessage(e);
                         }
                     }
                 }
-                sendGameResponseToBothPlayers(message);
+                sendGameResponseToBothPlayers(message,game);
             }
         }
     }
 
     private void handleInitialAction(InitialAction initialAction) throws IOException {
         logger.debug("handling initial action");
-        synchronized (Main.class) {
-            String name = initialAction.getName();
+        if (getGame() == null) {
+            synchronized (Main.class) {
+                String name = initialAction.getName();
 
-            int code = Main.getNameCount().getOrDefault(name, 0) +1;//codes will start with value 1
-            Main.getNameCount().put(name, code);
+                int code = Main.getNameCount().getOrDefault(name, 0) + 1;//codes will start with value 1
+                Main.getNameCount().put(name, code);
 
-            Player self = new Player(name, code, StoneState.NONE ,getClientSocket().getOutputStream());
-            Main.getPlayers().add(self);
-            this.setPlayer(self);
-            InitialResponse initialResponse = new InitialResponse(self);
-            sendResponse(self,initialResponse);
+                Player self = new Player(name, code, StoneState.NONE, getClientSocket().getOutputStream());
+                Main.getWaitingPlayers().add(self);
+                this.setPlayer(self);
+                InitialResponse initialResponse = new InitialResponse(self);
+                sendResponse(self, initialResponse);
+            }
         }
     }
 
     private void handleListPlayersAction(ListPlayersAction listPlayersAction) {
         logger.debug("handling listPlayers action");
-        synchronized (Main.class) {
-            Player self = getPlayerReference(listPlayersAction.getSelf());
-            if (Main.getWaitingPlayers().contains(self)) {
-                //should only work if the player itself is also not in a game
-                ArrayList<Player> waitingPlayers = new ArrayList<>(Main.getWaitingPlayers());
-                waitingPlayers.remove(self); //the asking player should not be included
+        if (getGame() == null) {
+            synchronized (Main.class) {
+                Player self = getPlayerReference(listPlayersAction.getSelf(),null);
+                if (Main.getWaitingPlayers().contains(self)) {
+                    //should only work if the player itself is also not in a game
+                    ArrayList<Player> waitingPlayers = new ArrayList<>(Main.getWaitingPlayers());
+                    waitingPlayers.remove(self); //the asking player should not be included
 
-                ListPlayersResponse listPlayersResponse = new ListPlayersResponse(waitingPlayers);
-                sendResponse(self,listPlayersResponse);
-            } else {
-                logger.warn("could not list players because requesting player {} is already in a game",self.getPlayerId());
+                    ListPlayersResponse listPlayersResponse = new ListPlayersResponse(waitingPlayers);
+                    sendResponse(self, listPlayersResponse);
+                } else {
+                    logger.warn("could not list players because requesting player {} is already in a game", self.getPlayerId());
+                }
             }
         }
     }
 
     private void handleReconnectAction(ReconnectAction reconnectAction) throws IOException {
         logger.debug("handling reconnect action");
-        Player self = getPlayerReference(reconnectAction.getPlayer());
+        Game game = getGame();
+        Player self = getPlayerReference(reconnectAction.getPlayer(), game);
 
-        Game game = findGame(self);
         if (game != null) {
-            this.setGame(game);
             synchronized (game) {
                 self.setOutputStream(getClientSocket().getOutputStream());
-                sendGameResponseToBothPlayers("Player " + self.getPlayerId() + " has reconnected.");
+                sendGameResponseToBothPlayers("Player " + self.getPlayerId() + " has reconnected.", game);
             }
         }
 
@@ -182,32 +184,35 @@ public class ClientHandler implements Runnable{
 
     private void handleEndSessionAction(EndSessionAction endSessionAction) throws IOException{
         logger.debug("handling endSession action");
-        if (getGame() != null) {
+        Game game = getGame();
+        if (game != null) {
             synchronized (Main.class) {
-                synchronized (getGame()) {
-                    Player self = getPlayerReference(endSessionAction.getPlayer());
-                    if (self.equals(getGame().getPlayer1()) || self.equals(getGame().getPlayer2())) {
+                synchronized (game) {
+                    Player self = getPlayerReference(endSessionAction.getPlayer(),game);
+                    if (self.equals(game.getPlayer1()) || self.equals(game.getPlayer2())) {
 
                         EndSessionResponse endSessionResponse = new EndSessionResponse(self.getPlayerId() + " hat das Spiel beendet.");
-                        sendResponseToBothPlayers(endSessionResponse);
+                        sendResponseToBothPlayers(endSessionResponse, game);
 
                         //make players waiting again
-                        getGame().getPlayer1().setColor(StoneState.NONE);
-                        getGame().getPlayer2().setColor(StoneState.NONE);
+                        game.getPlayer1().setColor(StoneState.NONE);
+                        game.getPlayer2().setColor(StoneState.NONE);
 
                         //make players place again
-                        getGame().getPlayer1().setPhase(GamePhase.PLACE);
-                        getGame().getPlayer2().setPhase(GamePhase.PLACE);
-
+                        game.getPlayer1().setPhase(GamePhase.PLACE);
+                        game.getPlayer2().setPhase(GamePhase.PLACE);
 
                         //reset placed stones to 0
-                        getGame().getPlayer1().setPlacedStones(0);
-                        getGame().getPlayer2().setPlacedStones(0);
+                        game.getPlayer1().setPlacedStones(0);
+                        game.getPlayer2().setPlacedStones(0);
+
+                        //add players to waiting players again
+                        Main.getWaitingPlayers().add(game.getPlayer1());
+                        Main.getWaitingPlayers().add(game.getPlayer2());
 
                         //remove game from Main datastructures
-                        Main.getGames().remove(getGame());
-                        //this removes the last local copy of the game Object. This also affects the thread of the other player
-                        this.setGame(null);
+                        Main.getGames().remove(game);
+
                     }
                 }
             }
@@ -217,9 +222,10 @@ public class ClientHandler implements Runnable{
 
     private void handleConnectAction(ConnectAction connectAction) {
         logger.debug("handling connect action");
+        if (getGame() == null) {
             synchronized (Main.class) {
-                Player self = getPlayerReference(connectAction.getSelf());
-                Player other = getPlayerReference(connectAction.getOther());
+                Player self = getPlayerReference(connectAction.getSelf(),null);
+                Player other = getPlayerReference(connectAction.getOther(),null);
 
                 if (Main.getRequestedPairs().containsKey(other) && Main.getRequestedPairs().get(other).equals(self)) {
                     //if the other player has already requested a game
@@ -235,13 +241,21 @@ public class ClientHandler implements Runnable{
                     }
                     Game game = new Game(self, other);
                     Main.getGames().add(game);
-                    this.setGame(game);
-                    String message = game.getNextPlayerToMove().getPlayerId() + " starts!";
-                    sendGameResponseToBothPlayers(message);
 
-                    //Main.getRequestedPairs().forEach((p1,p2)->System.out.println(p1.getPlayerId()+" "+p2.getPlayerId()));
+                    //removing players from waiting status
+                    Main.getWaitingPlayers().remove(self);
+                    Main.getWaitingPlayers().remove(other);
+
+                    //remove from requested pairs, since they are now in a game
                     Main.getRequestedPairs().remove(self);
                     Main.getRequestedPairs().remove(other);
+
+
+                    String message = game.getNextPlayerToMove().getPlayerId() + " starts!";
+                    synchronized (game) {
+                        sendGameResponseToBothPlayers(message, game);
+                    }
+                    //Main.getRequestedPairs().forEach((p1,p2)->System.out.println(p1.getPlayerId()+" "+p2.getPlayerId()));
                     //Main.getRequestedPairs().forEach((p1,p2)->System.out.println(p1.getPlayerId()+" "+p2.getPlayerId()));
 
                 } else {
@@ -249,44 +263,31 @@ public class ClientHandler implements Runnable{
                     //request a game with the player
                 }
             }
-    }
-
-    private Game findGame(Player player) {
-        synchronized (Main.class) {
-            for (Game game : Main.getGames()) {
-                synchronized (game) {
-                    if (player.equals(game.getPlayer1()) || player.equals(game.getPlayer2())) {
-                        return game;
-                    }
-                }
-            }
         }
-        return null;
     }
 
     private void sendResponse(Player player, Object response) {
-        synchronized (player) {
-            try {
-                ObjectOutputStream oos = new ObjectOutputStream(player.getOutputStream());
-                oos.writeObject(response);
-                oos.flush();
-                logger.debug("sent response to player {}",player.getPlayerId());
-            } catch (IOException e) {
-                logger.error("failed sending response to player {}",player.getPlayerId(),e);
-            }
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(player.getOutputStream());
+            oos.writeObject(response);
+            oos.flush();
+            logger.debug("sent response to player {}", player.getPlayerId());
+        } catch (IOException e) {
+            logger.error("failed sending response to player {}", player.getPlayerId(), e);
         }
+
     }
 
     /**
      * this method sends the passed in GameResponse directly to the Player this Thread is assigned to, and sends the same response with isYourTurn negated to the other player
      */
-    private void sendResponseToBothPlayers(Object response) {
-        sendResponse(getGame().getNextPlayerToMove() ,response);
-        sendResponse(getGame().getOtherPlayer(getGame().getNextPlayerToMove()) ,response);
+    private void sendResponseToBothPlayers(Object response, Game game) {
+        sendResponse(game.getNextPlayerToMove(), response);
+        sendResponse(game.getOtherPlayer(game.getNextPlayerToMove()), response);
     }
-    private void sendGameResponseToBothPlayers(String message) {
-        GameResponse gameResponse = new GameResponse(message,getNextAction(),getGame().getNextPlayerToMove(),getGame().getOtherPlayer(getGame().getNextPlayerToMove()),new ArrayList<>(getGame().getField().nodes()));
-        sendResponseToBothPlayers(gameResponse);
+    private void sendGameResponseToBothPlayers(String message, Game game) {
+        GameResponse gameResponse = new GameResponse(message, getNextAction(game), game.getNextPlayerToMove(), game.getOtherPlayer(game.getNextPlayerToMove()), new ArrayList<>(game.getField().nodes()));
+        sendResponseToBothPlayers(gameResponse, game);
     }
 
     private String getGameExceptionMessage(GameException e) {
@@ -298,34 +299,44 @@ public class ClientHandler implements Runnable{
             return  "You are not allowed to move to/from this position";
         } else return "An unknown GameException occurred";
     }
-    private ActionType getNextAction() {
-        if (getGame().isNextOperationTake()) {
-            return ActionType.TAKE;
-        } else if (getGame().getNextPlayerToMove().getPhase().equals(GamePhase.PLACE)) {
-            return ActionType.PLACE;
-        } else return ActionType.MOVE;
+    private ActionType getNextAction(Game game) {
+        if (game != null) {
+            if (game.isNextOperationTake()) {
+                return ActionType.TAKE;
+            } else if (game.getNextPlayerToMove().getPhase().equals(GamePhase.PLACE)) {
+                return ActionType.PLACE;
+            } else return ActionType.MOVE;
+        } else throw new InvalidPhaseException();
     }
 
     /**
      * should be used to get the locally stored Player Object from the passed in Player Object
      */
-    private Player getPlayerReference(Player player) {
-        synchronized (Main.class) {
-            return Main.getPlayers().stream().filter(p -> {
-                synchronized (p) {
-                    return p.equals(player);
-                }
-            }).findFirst().orElseThrow(IllegalPlayerException::new);
+    private Player getPlayerReference(Player player, Game game) {
+        if (game != null) {
+            synchronized (game) {
+                if (game.getPlayer1().equals(player)) {
+                    return game.getPlayer1();
+                } else if (game.getPlayer2().equals(player)) {
+                    return game.getPlayer2();
+                } else throw new IllegalPlayerException();
+            }
+        } else {
+            synchronized (Main.class) {
+                return Main.getWaitingPlayers().stream().filter(p -> p.equals(player)).findFirst().orElseThrow(IllegalPlayerException::new);
+            }
         }
     }
 
     private void sendDisconnectResponse() {
         logger.debug("handling disconnect response");
-        if (getGame()!= null) {
-            synchronized (getGame()) {
-                Player player = getGame().getOtherPlayer(getPlayer());
+        Game game = getGame();
+        if (game!= null) {
+            synchronized (game) {
+                Player player = game.getOtherPlayer(getPlayer());
                 sendResponse(player, new DisconnectResponse(player));
             }
         }
     }
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 import logic.entities.Game;
 import logic.entities.Player;
-import logic.entities.StoneState;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,18 +8,20 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.*;
-import java.util.stream.Collectors;
+
 
 public class Main {
     private static final  List<Game> GAMES = Collections.synchronizedList(new ArrayList<>());
-    private static final List<Player> PLAYERS = Collections.synchronizedList(new ArrayList<>());
+    private static final List<Player> WAITING_PLAYERS = Collections.synchronizedList(new ArrayList<>());
     private static final Map<String, Integer> NAME_COUNT = Collections.synchronizedMap(new HashMap<>());
     private static final Map<Player,Player> REQUESTED_PAIRS = Collections.synchronizedMap(new HashMap<>());
 
     public static void main(String[] args) throws IOException {
 
         Logger logger = LoggerFactory.getLogger(Main.class);
-        ServerSocket serverSocket = new ServerSocket(5056);
+        int port =5056;
+        ServerSocket serverSocket = new ServerSocket(port);
+        logger.info("the server is listening to requests on port {}",port);
         Socket currentSocket;
         while(true) {
             try {
@@ -37,12 +39,10 @@ public class Main {
     public static List<Game> getGames() {
         return GAMES;
     }
-    public static List<Player> getPlayers() {
-        return PLAYERS;
-    }
     public static List<Player> getWaitingPlayers() {
-        return getPlayers().stream().filter(p -> p.getColor().equals(StoneState.NONE)).collect(Collectors.toList());
+        return WAITING_PLAYERS;
     }
+
     public static Map<String, Integer> getNameCount() {
         return NAME_COUNT;
     }
